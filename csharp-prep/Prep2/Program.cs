09a4847288df8a7c1b1e1031b6b8986ceba0d3d5
rdomon <dom21008@byui.edu>
2024-06-30 14:05:33 +0900

    did prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,10 +1,48 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
         //This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+         else if (percent >= 70)
+        {
+            letter = "C";
+        }
+         else if (percent >= 60)
+        {
+            letter = "D";
+        }
+         else
+         {
+            letter = "F";
+         }
+
+         Console.WriteLine($"Your grade is: {letter}.");
+
+         if (percent >= 70)
+         {
+            Console.WriteLine("Congratulation! You passed the class:)");
+         }
+         else{
+            Console.WriteLine("You could not passed the class. Good luck with next time!");
+
+         }
+
     }
 }
