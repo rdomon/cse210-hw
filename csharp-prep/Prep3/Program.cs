e338011a37f9f96847da2cc295c9e55e5bc7fc1f
rdomon <dom21008@byui.edu>
2025-02-25 13:09:07 -0700

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,25 +5,34 @@ class Program
     static void Main(string[] args)
     {
         // ask the magic number
-        Console.Write("Enter the magic number: ");
-        int magicNumber = int.Parse(Console.ReadLine());
-
-        // ask the guess
-        Console.Write("What is your guess? ");
-        int guess = int.Parse(Console.ReadLine());
+        // Console.Write("Enter the magic number: ");
+        // int magicNumber = int.Parse(Console.ReadLine());
 
 
-        if (guess < magicNumber)
+        // make it loop
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
         {
-            Console.WriteLine("Higher");
-        }
-        else if (guess > magicNumber)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!!");
+            // ask the guess
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!");
+            }
         }
     }
 }
